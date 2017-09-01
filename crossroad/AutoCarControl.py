d9f22bc77416139ed64efeecd3479fd7a994739c
lvlvlvlvlv <lv_xubo@163.com>
2017-09-01 09:35:06 +0800

    improve traci-python control part

--- crossroad/AutoCarControl.py
+++ crossroad/AutoCarControl.py
@@ -3,20 +3,28 @@
 import os, sys
 import subprocess
 import traci
-import traci.constants as tc
-import traci._vehicle as tv
+
+
+
 PORT = 8813
 sumoBinary = "/usr/bin/sumo-gui"
 sumoProcess = subprocess.Popen([sumoBinary, "-c", "crossroad.sumocfg", "--remote-port", str(PORT)], stdout=sys.stdout, stderr=sys.stderr)
 
 
 
+vd = traci._vehicle.VehicleDomain()
 traci.init(PORT)
+
+
+
 step = 0
-AutoCar = tv.VehicleDomain()
-AutoCar.setStop("Auto","-gneE0_0")
 while step < 10000:
-    print "here we take the control"
+    print "Here we take the control !!"
+    if step % 100 == 0:
+        vid = "new" + str(step)
+        vd.add(vid, "auto route",typeID="trailer")
+
+
     traci.simulationStep()
 
     step += 1
